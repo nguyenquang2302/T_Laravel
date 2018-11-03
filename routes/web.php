f0b593cd1c8636c74771eb912049fdc54907b14b
quang.nd <quang.nd@phuongnamdigital.com>
2018-11-03 11:37:45 +0700

    Router Thông thường

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// Routing
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+// Routing sub path
+Route::get('/hello/slug', function () {
+    return 'Học laravel';
+});
+// có kèm slug
+Route::get('/xinchao/{name}', function ($name) {
+    return "Xin chào $name";
+});
+
+//  Default slug ( có hoặc không có)
+Route::get('/laptrinh/{name?}', function ($name = 'php') {
+    return "Lập trình $name";
+});
+
+//  Điều kiện cho router ( điều kiện cho slug)
+Route::get('/thong-tin/{ten}/{tuoi}', function ($ten, $tuoi) {
+    return "Xin chào $ten - -  $tuoi";
+})->where(['ten' => '[a-zA-z]+', 'tuoi' => '[0-9]+']);
+
+// 
