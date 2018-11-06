564271fc35e75d7d9ffa8a77e084b6ab64295bbc
quang.nd <quang.nd@phuongnamdigital.com>
2018-11-06 10:58:01 +0700

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+            'name' => 'ĐQ',
+            'email' => 'dq@gmail.com',
+            'password' => bcrypt('123456') 
+        ]);
+
     }
 }
